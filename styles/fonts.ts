f2d09f8104183d7fde8afe9ba58dc414e058e18d
inontz <inontz@icloud.com>
2023-06-19 02:37:37 +0700

    Init web first looks

---
+++ styles/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const lineSeedRg = localFont({ src: './LINESeedSansTH_W_Rg.woff2' })
+const lineSeedBd = localFont({ src: './LINESeedSansTH_W_Bd.woff2' })
+const lineSeedTh = localFont({ src: './LINESeedSansTH_W_Th.woff2' })
+const lineSeedHe = localFont({ src: './LINESeedSansTH_W_He.woff2' })
+const lineSeedXBd = localFont({ src: './LINESeedSansTH_W_XBd.woff2' })
+
+export { inter, lineSeedRg, lineSeedBd, lineSeedXBd, lineSeedTh, lineSeedHe}
