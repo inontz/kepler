fd694e640dd138a940449d5032a03f9b452bfcec
inontz <inontz@icloud.com>
2023-06-19 04:25:38 +0700

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,61 @@
 import Image from 'next/image'
 
+const callouts = [
+  {
+    name: 'Kepler',
+    description: 'Work from home accessories',
+    imageSrc: '/image/AstroBoy.png',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: '#',
+  },
+  {
+    name: 'API',
+    description: 'Journals and note-taking',
+    imageSrc: '/image/AstroBoy.png',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: '#',
+  },
+  {
+    name: 'Berlin',
+    description: 'Daily commute essentials',
+    imageSrc: '/image/AstroBoy.png',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: '#',
+  },
+]
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      
+    <main className="min-h-screen items-center justify-center">
+      <div className="bg-white">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto min-h-screen max-w-2xl sm:py-24 lg:max-w-none">
+            <h2 className="text-2xl font-bold text-gray-900">Portal 🧑‍🚀</h2>
+
+            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+              {callouts.map((callout) => (
+                <div key={callout.name} className="group relative">
+                  <div className="relative h-24 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                    <Image
+                      width={885}
+                      height={792}
+                      src={callout.imageSrc}
+                      alt={callout.imageAlt}
+                      className="h-full w-full object-cover object-center"
+                    />
+                  </div>
+                  <h3 className="mt-6 text-sm text-gray-500">
+                    <a href={callout.href}>
+                      <span className="absolute inset-0" />
+                      {callout.name}
+                    </a>
+                  </h3>
+                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
