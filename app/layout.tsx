bb145e6ddbf5d3b6fee84df8732d8f66522cfcd7
inontz <inontz@icloud.com>
2023-06-21 04:07:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-"use client"
 import "./globals.css";
 import { lineSeedRg } from "@/fonts";
-import { InitLiff } from "./hooks/useLine";
-import liff from "@line/liff";
 
 export const metadata = {
   title: "Kepler 🚀",
